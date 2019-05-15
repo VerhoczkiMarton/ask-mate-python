d4c7ffe0b2cb2021e93ef1d0188828ae93f625d4
Enci21 <eni.hegedus.95@gmail.com>
2019-05-15 16:02:23 +0200

    ask a question is ok

--- server.py
+++ server.py
@@ -44,10 +44,24 @@ def new_answer(question_id):
         return render_template('new_answer.html',question_id=question_id )
 
 
-@app.route('/add-question')
-def add_question():
-    pass
-
+@app.route('/add-question', methods=['GET', 'POST'])
+def new_question():
+    if request.method == 'POST':
+        new_question = dict()
+        id = str(uuid.uuid4())
+        new_question['submission_time'] = datetime.datetime.now().timestamp()
+        new_question['view_number'] = 0
+        new_question['vote_number'] = 0
+        new_question['title'] = request.form.get('title')
+        new_question['message'] = request.form.get('message')
+        new_question['image'] = None
+        global new_question
+        questions.update({id: new_question})
+        connection.write_all_questions(questions)
+        questions = data_manager.get_questions()
+        return redirect(f'/question/{question_id}')
+    elif request.method == 'GET':
+        return render_template('add_question.html', question_id=question_id)
 
 if __name__ == '__main__':
     app.run(
