072a88d91d173233e89a0a2c187311bcd65eb751
verho <verhoczki.marci@gmail.com>
2019-05-15 15:51:14 +0200

    Implement Datetime in human readable form

--- server.py
+++ server.py
@@ -16,11 +16,13 @@ ANSWER_HEADERS = ['Submission time', 'Vote number', 'Question id', 'Message', 'I
 @app.route('/list')
 def route_list():
     questions = data_manager.get_questions()
+    util.convert_timestamp(questions, answers)
     return render_template('list.html', questions=questions, headers = QUESTION_HEADERS)
 
 
 @app.route('/question/<question_id>')
 def display_question(question_id):
+    util.convert_timestamp(questions, answers)
     question = questions[question_id]
     answers_for_question = util.get_answers_for_question_id(question_id, answers)
     return render_template('question.html', question=question, question_id=question_id, answers=answers_for_question)
@@ -30,7 +32,7 @@ def new_answer(question_id):
     if request.method == 'POST':
         new_answer = dict()
         id = str(uuid.uuid4())
-        new_answer['submission_time'] = datetime.datetime.now().timestamp()
+        new_answer['submission_time'] = round(datetime.datetime.now().timestamp())
         new_answer['vote_number'] = 0
         new_answer['question_id'] = question_id
         new_answer['message'] = request.form.get('message')
