1a1032c6523efee898a16d5a0bdb6c87208a5617
verho <verhoczki.marci@gmail.com>
2019-05-15 17:37:15 +0200

    Add description to util.py

--- server.py
+++ server.py
@@ -7,8 +7,10 @@ import datetime
 
 
 app = Flask(__name__)
+
 questions = data_manager.get_questions()
 answers = data_manager.get_answers()
+
 QUESTION_HEADERS = ['Submission time', 'View number', 'Vote number', 'Title', 'Message', 'Image']
 ANSWER_HEADERS = ['Submission time', 'Vote number', 'Question id', 'Message', 'Image']
 
@@ -16,12 +18,14 @@ ANSWER_HEADERS = ['Submission time', 'Vote number', 'Question id', 'Message', 'I
 @app.route('/list')
 def route_list():
     questions = data_manager.get_questions()
-    return render_template('list.html', questions=questions, headers = QUESTION_HEADERS)
+    return render_template('list.html', questions=questions, headers=QUESTION_HEADERS)
 
 
 @app.route('/question/<question_id>')
 def display_question(question_id):
+    questions = data_manager.get_questions()
     answers_for_question = util.get_answers_for_question_id(question_id, answers)
+
     return render_template('question.html', question_id=question_id, answers=answers_for_question, questions=questions)
 
 
@@ -30,13 +34,15 @@ def new_answer(question_id):
     if request.method == 'POST':
         new_answer = dict()
         id = str(uuid.uuid4())
+
         new_answer['submission_time'] = round(datetime.datetime.now().timestamp())
         new_answer['vote_number'] = 0
         new_answer['question_id'] = question_id
         new_answer['message'] = request.form.get('message')
         new_answer['image'] = None
+
         global answers
-        answers.update({id : new_answer})
+        answers.update({id: new_answer})
         connection.write_all_answers(answers)
         answers = data_manager.get_answers()
         return redirect(f'/question/{question_id}')
@@ -49,6 +55,7 @@ def new_question():
     if request.method == 'POST':
         new_question = dict()
         id = str(uuid.uuid4())
+
         new_question['id'] = id
         new_question['submission_time'] = round(datetime.datetime.now().timestamp())
         new_question['view_number'] = 0
@@ -56,6 +63,7 @@ def new_question():
         new_question['title'] = request.form.get('title')
         new_question['message'] = request.form.get('message')
         new_question['image'] = None
+
         global questions
         questions.update({id: new_question})
         connection.write_all_questions(questions)
--- util.py
+++ util.py
@@ -1,8 +1,9 @@
-import datetime
-
-
 def get_answers_for_question_id(question_id, answers):
+    """
+    return a list of dictionaries, containing the answers to a specified question id
+    """
     answers_for_question_id = []
+
     for answer in answers.values():
         if str(answer['question_id']) == question_id:
             answers_for_question_id.append(answer)
