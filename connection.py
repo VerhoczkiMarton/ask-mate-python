5b47d0858158b834b58e6a2423a2bc6aa5409215
verho <verhoczki.marci@gmail.com>
2019-05-15 12:09:45 +0200

    Add get_answers, get_questions to data_manager

---
+++ connection.py
@@ -0,0 +1,21 @@
+from csv import DictReader, DictWriter
+
+
+def get_all_answers():
+    all_answers = []
+    input_file = DictReader(open("answers.csv"))
+    for line in input_file:
+        all_answers.append(line)
+    return all_answers
+
+
+def get_all_questions():
+    all_questions = []
+    input_file = DictReader(open("questions.csv"))
+    for line in input_file:
+        all_questions.append(line)
+    return all_questions
+
+
+def write_all():
+    pass
--- data_manager.py
+++ data_manager.py
@@ -1,8 +1,30 @@
 import connection
 
+
 def get_answers():
-    pass
+    list_of_all_answers = connection.get_all_answers()
+    answers = dict()
+    for answer in list_of_all_answers:
+        id = answer.pop('id')
+        answer = parse_all_ints(answer)
+        answers.update({id: answer})
+    return answers
 
 
 def get_questions():
-    pass
+    list_of_all_questions = connection.get_all_questions()
+    questions = dict()
+    for question in list_of_all_questions:
+        id = question.pop('id')
+        question = parse_all_ints(question)
+        questions.update({id: question})
+    return questions
+
+
+def parse_all_ints(dict_):
+    for key, value in dict_.items():
+        try:
+            dict_[key] = int(value)
+        except ValueError:
+            continue
+    return dict_
