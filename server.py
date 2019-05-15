f9c7682e6f60d60eefdc9a7048c181c1ff615b9e
verho <verhoczki.marci@gmail.com>
2019-05-15 15:10:59 +0200

    Implement post an answer

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 import data_manager
 import connection
+import util
+import uuid
+import datetime
+
 
 app = Flask(__name__)
 questions = data_manager.get_questions()
@@ -20,7 +24,26 @@ def route_list():
 @app.route('/question/<question_id>')
 def display_question(question_id):
     question = questions[question_id]
-    return render_template('question.html', question=question, question_id=question_id, question_headers=QUESTION_HEADERS)
+    answers_for_question = util.get_answers_for_question_id(question_id, answers)
+    return render_template('question.html', question=question, question_id=question_id, answers=answers_for_question)
+
+@app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
+def new_answer(question_id):
+    if request.method == 'POST':
+        new_answer = dict()
+        id = str(uuid.uuid4())
+        new_answer['submission_time'] = datetime.datetime.now().timestamp()
+        new_answer['vote_number'] = 0
+        new_answer['question_id'] = question_id
+        new_answer['message'] = request.form.get('message')
+        new_answer['image'] = None
+        global answers
+        answers.update({id : new_answer})
+        connection.write_all_answers(answers)
+        answers = data_manager.get_answers()
+        return redirect(f'/question/{question_id}')
+    else:
+        return render_template('new_answer.html',question_id=question_id )
 
 
 @app.route('/add-question')
