c2e4e6646d1c4b9156c3530925b2fb36646e5292
verho <verhoczki.marci@gmail.com>
2019-05-15 17:25:44 +0200

    Kinda working state, A new beginning

--- connection.py
+++ connection.py
@@ -18,20 +18,30 @@ def get_all_questions():
 
 def write_all_questions(questions):
     fieldnames = get_headers('questions')
+    questions_list = []
+    for id, content in questions.items():
+        temp_dict = dict(content.items())
+        temp_dict.update({'id': id})
+        questions_list.append(temp_dict)
+
     with open('questions.csv', 'w') as csvfile:
         writer = DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for question in questions.values():
+        for question in questions_list:
             writer.writerow(question)
 
 
 def write_all_answers(answers):
     fieldnames = get_headers('answers')
+    answers_list = []
+    for id, content in answers.items():
+        temp_dict = dict(content.items())
+        temp_dict.update({'id': id})
+        answers_list.append(temp_dict)
     with open('answers.csv', 'w') as csvfile:
         writer = DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for id, answer in answers.items():
-            answer['id'] = id
+        for answer in answers_list:
             writer.writerow(answer)
 
 
--- util.py
+++ util.py
@@ -4,6 +4,6 @@ import datetime
 def get_answers_for_question_id(question_id, answers):
     answers_for_question_id = []
     for answer in answers.values():
-        if answer['question_id'] == int(question_id):
+        if str(answer['question_id']) == question_id:
             answers_for_question_id.append(answer)
     return answers_for_question_id
